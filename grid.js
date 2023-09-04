c196e77f855c2c8987c4a23d3857acf38554888d
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-09-04 14:59:18 +0100

    First Commit

---
+++ grid.js
@@ -0,0 +1,24 @@
+// const grid = []
+
+// for (let i = 0; i < 3; i++) {
+//     grid[i] = []
+//     for (let j = 0; j < 3; j++) {
+//         grid[i][j] = null;
+//     }
+// }
+
+// console.log(grid)
+
+
+
+console.log("    |     |    ")
+console.log("    |     |    ")
+console.log("    |     |    ")
+console.log("---------------")
+console.log("    |     |    ")
+console.log("    |     |    ")
+console.log("    |     |    ")
+console.log("---------------")
+console.log("    |     |    ")
+console.log("    |     |    ")
+console.log("    |     |    ")
---
+++ variables.js
@@ -0,0 +1,42 @@
+console.log("this is my variables file");
+
+// let
+// const
+// var 
+
+let myName = "Jazmyne";
+let myAge = 29;
+let gotAHeadache = true;
+const bornIn = "Osaka"; //const keeps locked
+
+console.log(myName);
+
+myName = "Jake";
+
+console.log(myName);
+
+// bornIn = "Japan"
+
+// var myAddress = "North West"
+
+console.log(2+5)
+console.log(7-5)
+console.log(3*3)
+console.log(3**3)
+console.log(15/3)
+console.log(15%3) //modulus operator //useful for pagination
+
+let myNum = 4
+
+myNum++ //increment
+
+console.log(myNum) //should be 5
+
+let balance = 100
+
+let withdrawalAmount = 30
+
+console.log(balance = balance - withdrawalAmount)
+
+console.log(balance)
+
