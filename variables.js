a69b118cb8b22b75840851171ef987cf7d423854
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-09-05 10:53:32 +0100

    Rename files with activities 3/4 done

--- variables.js
+++ variables.js
@@ -2,7 +2,7 @@ console.log("this is my variables file");
 
 // let
 // const
-// var 
+// var
 
 let myName = "Jazmyne";
 let myAge = 29;
@@ -19,24 +19,36 @@ console.log(myName);
 
 // var myAddress = "North West"
 
-console.log(2+5)
-console.log(7-5)
-console.log(3*3)
-console.log(3**3)
-console.log(15/3)
-console.log(15%3) //modulus operator //useful for pagination
+console.log(2 + 5);
+console.log(7 - 5);
+console.log(3 * 3);
+console.log(3 ** 3);
+console.log(15 / 3);
+console.log(15 % 3); //modulus operator //useful for pagination
 
-let myNum = 4
+let myNum = 4;
 
-myNum++ //increment
+myNum++; //increment
 
-console.log(myNum) //should be 5
+console.log(myNum); //should be 5
 
-let balance = 100
+let balance = 100;
 
-let withdrawalAmount = 30
+let withdrawalAmount = 30;
 
-console.log(balance = balance - withdrawalAmount)
+console.log((balance = balance - withdrawalAmount));
+//need = which is the assignment operator for new balance
 
-console.log(balance)
+console.log(balance);
 
+balance -= withdrawalAmount;
+
+let favouriteDrink = "water";
+
+console.log(
+  "My name is " + myName + ". My favourite drink is " + favouriteDrink
+); //this is concatination
+
+console.log(`My name is ${myName} and my favourite drink is ${favouriteDrink}`); // this is an easier way of doing concatination and it is a template literal
+
+//it is good to use delimiters which is a semicolon at the end of each script
